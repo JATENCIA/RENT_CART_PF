7105b5203e45053579d46e2bbaa25bd290467b16
JATENCIA <atenciajaramilloj@gmail.com>
2023-02-18 09:50:04 -0500

    update

--- Api/Src/Routes/Payment.js
+++ Api/Src/Routes/Payment.js
@@ -7,7 +7,7 @@ const PaymentService = require("../services/PaymentService");
 const PaymentInstance = new PaymentController(new PaymentService());
 
 router.post("/", function (req, res, next) {
-  PaymentInstance.getPaymentLink(req, res);  
+  PaymentInstance.getPaymentLink(req, res);
 });
 
 module.exports = router;
