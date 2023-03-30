0d94a0e2c889991b647de69d40a484beeb77cdad
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-03-29 22:08:00 -0500

    ..

--- client/src/Components/Auth0/ProtectedRoute.jsx
+++ client/src/Components/Auth0/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
-function ProtectedRoute({ isAuthenticated, children, redirectTo = "/" }) {
+function ProtectedRoute({ isAuthenticated, children, redirectTo = "/home" }) {
   if (!isAuthenticated) {
     return <Navigate to={redirectTo} />;
   }
